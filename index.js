c240451bb1196e1265f92b89c592f96140e6426d
Otoniel Reyes <otonielreyesgalay@gmail.com>
2020-08-22 04:52:39 -0400

    Include new boilerplate for a simple vue powered page.
    Cli tool modularized to improve speed and performance.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const help = require('./lib/help.js');
 const templates = require('./lib/templates.js');
-const tp = templates;
 const process = require('process');
 const fs = require('fs');
 const https = require('https')
@@ -12,35 +11,21 @@ if (process.argv[2]){
 		case "webpage": {
 			if (process.argv[3]){
 				// generate the webpage
-				let tree = tp.webpageTree;
-				let project = process.argv[3];
-				fs.mkdirSync(`./${project}`);
+				let webpage = require('./lib/webpage.js');
+				let tree = webpage.tree;
+
+				let projectName = process.argv[3];
+				
+				fs.mkdirSync(`./${projectName}`);
 				for (i = 0; i < tree.length; i++){
-					fs.mkdirSync(tree[i]);
+					fs.mkdirSync(projectName+tree[i]);
 				}
 				console.log(`Folder tree successfully created!`);
-				fs.writeFile(`./${project}/index.html`, tp.simpleHtml, (err)=>{
-					if (err) throw err;
-				});
-				fs.writeFile(`./${project}/404.html`, tp._404Page, (err)=>{
-					if (err) throw err;
-				});
-				fs.writeFile(`./${project}/robots.txt`, tp.robotsTXT, (err)=>{
-					if (err) throw err;
-				});
-				fs.writeFile(`./${project}/humans.txt`, tp.humansTXT, (err)=>{
-					if (err) throw err;
-				});
-				fs.writeFile(`${tree[0]}/master.css`, tp.resetCSS, (err)=>{
-					if (err) throw err;
-				});
-				fs.writeFile(`${tree[2]}/main.js`, tp.simpleJS, (err)=>{
-					if (err) throw err;
-				});
-				tp.JQuery(tree[3]);
-				tp.JQueryMin(tree[3]);
+
+				webpage.createFiles();
+
 				console.log(`Files successfully created!`);
-				console.log(`Project ${project} ready to work on!`);
+				console.log(`Project ${projectName} ready to work on!`);
 				break;
 			}
 		}
